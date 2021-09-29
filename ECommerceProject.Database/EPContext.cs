a72955a0c218979ddd188b12fd4d6e89e80069fb
Md Tabrez Alam <alamtabrez628@gmail.com>
2021-09-29 12:56:26 +0530

    Initilization Project Using Entity Framework in ECommerceProject.Database And Create two project ECommerceProject.Database , ECommerceProject.Entity And In ECommerceProject.Entity We Create Three Class Category,ProductAnd BaseEntity

---
+++ ECommerceProject.Database/EPContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProject.Database
+{
+    public class EPContext: DbContext
+    {
+        public EPContext(): base("ECommerceConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
