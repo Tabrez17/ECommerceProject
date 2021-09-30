62dbec78aebfe71fb6a380c2ebefd5229c35dfbb
Md Tabrez Alam <alamtabrez628@gmail.com>
2021-09-30 12:20:56 +0530

    Create a Project ECommerceProject.Service And Create a CRUD Operation For Product Without Using JavaScript

---
+++ ECommerceProject.Services/CategoriesService.cs
@@ -0,0 +1,56 @@
+﻿using ECommerceProject.Database;
+using ECommerceProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProject.Services
+{
+    public class CategoriesService
+    {
+        public Category GetCategoryById(int ID)
+        {
+            using (var context = new EPContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        public List<Category> GetCategory()
+        {
+            using (var context = new EPContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            using (var context = new EPContext())
+            {
+                context.Categories.Add(category);
+
+                context.SaveChanges();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new EPContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void deleteCategory(int ID)
+        {
+            using (var context = new EPContext())
+            {
+               var category = context.Categories.Find(ID);
+                context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+
+                //context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ECommerceProject/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using ECommerceProject.Entity;
+using ECommerceProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategory();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var categories = categoryService.GetCategoryById(ID);
+            return View(categories);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var categories = categoryService.GetCategoryById(ID);
+            return View(categories);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoryService.deleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+    }
+}
